5e8876715e9f8a85d63231b20b2f13ead1619e85
SivaSampath <76162764+SivaSampath@users.noreply.github.com>
2020-12-25 21:26:14 +0530

    Add files via upload

---
+++ IF-ELSE-Statement.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"Enter a Number:\n";
+    cin>>i;
+    if(i>=10)
+    {
+        cout<<i<<" is greater than or equal to 10\n";
+    }
+    else
+    {
+        cout<<i<<" is lesser than 10\n";
+    }
+    return 0;
+}
+
+    
+
