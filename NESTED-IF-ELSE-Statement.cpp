d38b0b68b2a5e5dc6f177a27fd019b98dc0fbbf8
SivaSampath <76162764+SivaSampath@users.noreply.github.com>
2020-12-25 21:27:13 +0530

    Add files via upload

---
+++ NESTED-IF-ELSE-Statement.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"Enter a number:\n";
+    cin>>i;
+    if(i>=10)
+    {
+        if(i>10)
+        {
+            cout<<i<<" is greater than 10\n";
+        }
+        else
+        {
+            cout<<i<<" is equal to 10\n";
+        }
+    }
+    else
+    {
+        cout<<i<<" is lesser than 10\n";
+    }
+    return 0;
+}
